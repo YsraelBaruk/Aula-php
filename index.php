b5d9f898c7c650ed89d3d79fc5b43a147b6bd1a1
YsraelBaruk <81303434+YsraelBaruk@users.noreply.github.com>
2022-04-07 14:48:56 -0300

    learning array

--- index.php
+++ index.php
@@ -4,10 +4,46 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">in
+    <link rel="stylesheet" href="style.css">
     <title>Batata Ltda.</title>
 </head>
 <body>
-    <h1>Bom dia</h1>
+    <?php
+        $produto = [
+            [
+                'categora' => 'computadores',
+                'itens' => [
+                    'PC Gamer HP',
+                    'Notebook Lenovo',
+                    'Notebook Sansung',
+                    'Asus VivoBook'
+                ]
+            ],
+            [
+                'categora' => 'periféricos',
+                'itens' => [
+                    'Mouse',
+                    'Monitor',
+                    'Fone de Ouvido',
+                    'Teclado',
+                    'ReadSet'
+                ]
+            ]
+        ];
+
+        foreach($produto as $categoria){
+            echo '<h2>'.$Categoria['categoria'].'</h2>';
+            echo '<ul>';
+            foreach($categoria['itens'] as $produto){
+                echo '<li>'.$produto.'</li>';
+            }
+            echo '</ul>';
+        }
+
+        // for($id = 0; $id < count($produto); $id++){
+        //     echo '<p> id: '.$id.' -> '.$produto[$id].'</p>';
+        // }
+    ?>
+    
 </body>
 </html>
